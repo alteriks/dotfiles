1c08a5a3bc6b66f465eb9f632cb3d1f888cc74ea
Krzysztof Dajka <alteriks@protonmail.com>
2020-11-24 13:02:52 +0100

    auto-commit-moar

--- .config/i3/bin/launch_and_rename_windowname.sh
+++ .config/i3/bin/launch_and_rename_windowname.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 NAME=$(echo $* | egrep -o "scratchpad-\w+")
-exec $* &
-CMD_PID=$!
-usleep 200
-xdotool set_window --name $NAME $(xdotool search --pid $CMD_PID | head -1)
-i3-msg "[instance=$NAME] move to scratchpad"
+if [[  $(pgrep --count --full $NAME) -gt 1 ]]; then
+  #above condition matches this script so we need to use count >= 1
+  echo "$NAME is running, nothing to do."
+else
+  exec $* &
+  CMD_PID=$!
+  usleep 200
+  xdotool set_window --name $NAME $(xdotool search --pid $CMD_PID | head -1)
+  i3-msg "[instance=$NAME] move to scratchpad"
+fi
