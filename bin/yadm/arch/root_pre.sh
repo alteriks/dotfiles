346918781d573aecd4ad328e5df62599c3695d24
Krzysztof Dajka <alteriks@protonmail.com>
2021-03-31 16:53:20 +0200

    auto-commit carbon

--- bin/yadm/arch/root_pre.sh
+++ bin/yadm/arch/root_pre.sh
@@ -10,7 +10,11 @@ set -eo pipefail
 #   kernel-alive
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+source $SCRIPTPATH/privenv
+
 STRIP="sed -e '/^#/d' -e 's/\s*#.*//' -e '/^\s*$/d'"
 PACKAGES="$SCRIPTPATH/pacman/all ${SCRIPTPATH}/pacman/*${HOSTNAME}*"
 INSTALL_CMD="pacman -Syu --noconfirm --needed -"
 cat $(ls $PACKAGES ) | eval $STRIP | eval $INSTALL_CMD
+
+step-cli ca bootstrap --ca-url $CA_URL --install --fingerprint $CA_FINGERPRINT
