473466b3f8303a8266fb121d355793e26a96f4b1
Krzysztof Dajka <alteriks@protonmail.com>
2021-11-06 13:48:04 +0100

    auto-commit moar

---
+++ .config/i3/bin/setxkbmap.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Using '-option' to clear previous settings
+setxkbmap -option -option terminate:ctrl_alt_bksp -option kpdl:dot -option caps:escape -layout pl
---
+++ .config/i3/bin/setxkbmap_inputplug.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# XInput events monitor daemon - https://github.com/andrewshadura/inputplug
+# install inputplug with yay
+
+{ echo "XIDeviceEnabled XISlaveKeyboard"; inputplug -d -c /bin/echo; } |
+while read event
+do
+        case $event in
+        XIDeviceEnabled*XISlaveKeyboard*)
+                ~/.config/i3/bin/setxkbmap.sh
+                ;;
+        esac
+done
