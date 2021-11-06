4b0591e8bdf24642d1f1919c336061d4b9ddd0f3
Krzysztof Dajka <alteriks@protonmail.com>
2021-11-06 14:24:48 +0100

    auto-commit moar

--- .config/i3/bin/setxkbmap_inputplug.sh
+++ .config/i3/bin/setxkbmap_inputplug.sh
@@ -1,13 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 # XInput events monitor daemon - https://github.com/andrewshadura/inputplug
 # install inputplug with yay
 
-{ echo "XIDeviceEnabled XISlaveKeyboard"; inputplug -d -c /bin/echo; } |
-while read event
-do
-        case $event in
+NAME="$(basename $0)"
+if [[  $(pgrep --count --full "$NAME") -gt 1 ]]; then
+  echo "Already running: $NAME"
+else
+  { echo "XIDeviceEnabled XISlaveKeyboard"; inputplug -d -c /bin/echo; } |
+    while read event
+    do
+      case $event in
         XIDeviceEnabled*XISlaveKeyboard*)
-                ~/.config/i3/bin/setxkbmap.sh
-                ;;
-        esac
-done &
+          ~/.config/i3/bin/setxkbmap.sh
+          ;;
+      esac
+    done &
+fi
