3f3bfd4dd22d0b59d44a33324c78bb13861a09cd
Krzysztof Dajka <alteriks@protonmail.com>
2022-08-22 21:39:23 +0200

    auto-commit moar

--- .config/xplr/init.lua
+++ .config/xplr/init.lua
@@ -1,4 +1,47 @@
 version = "0.19.0"
+-- The default layout without SortAndFilter
+--
+-- Type: [Layout](https://xplr.dev/en/layout)
+xplr.config.layouts.builtin.default = {
+  Horizontal = {
+    config = {
+      constraints = {
+        { Percentage = 70 },
+        { Percentage = 30 },
+      },
+    },
+    splits = {
+      {
+        Vertical = {
+          config = {
+            constraints = {
+              { Min = 1 },
+              { Length = 3 },
+            },
+          },
+          splits = {
+            "Table",
+            "InputAndLogs",
+          },
+        },
+      },
+      {
+        Vertical = {
+          config = {
+            constraints = {
+              { Percentage = 30 },
+              { Percentage = 70 },
+            },
+          },
+          splits = {
+            "Selection",
+            "HelpMenu",
+          },
+        },
+      },
+    },
+  },
+}
 
 local home = os.getenv("HOME")
 local xpm_path = home .. "/.local/share/xplr/dtomvan/xpm.xplr"
@@ -32,13 +75,14 @@ require("xpm").setup({
     'sayanarijit/map.xplr',
     'sayanarijit/qrcp.xplr',
     'sayanarijit/zoxide.xplr',
+    -- 'sayanarijit/material-landscape.xplr',
   },
   auto_install = true,
   auto_cleanup = true,
 })
 
 require("qrcp").setup{
-  mode = "action",
+  mode = "default",
   key = "Q",
   send_options = "-i lan",
   receive_options = "-i lan",
@@ -72,3 +116,92 @@ require("nuke").setup{
   }
   key["f3"] = xplr.config.modes.custom.nuke.key_bindings.on_key.v
   key["enter"] = xplr.config.modes.custom.nuke.key_bindings.on_key.o
+  -- Copied from /usr/share/xplr/examples/init.lua, without help = "XXXX"
+  key.G = {
+    messages = {
+      "PopMode",
+      "FocusLast",
+    },
+  }
+  key.down = {
+    messages = {
+      "FocusNext",
+    },
+  }
+  key.j = {
+    messages = {
+      "FocusNext",
+    },
+  }
+  key.up = {
+    messages = {
+      "FocusPrevious",
+    },
+  }
+  key.k = {
+    messages = {
+      "FocusPrevious",
+    },
+  }
+  key.right = {
+    messages = {
+      "Enter",
+    },
+  }
+  key.l = {
+    messages = {
+      "Enter",
+    },
+  }
+  key.left = {
+    messages = {
+      "Back",
+    },
+  }
+  key.h = {
+    messages = {
+      "Back",
+    },
+  }
+
+require("zoxide").setup{
+  mode = "default",
+  key = "z",
+}
+
+-- require("material-landscape").setup{
+--   keep_default_layout = true
+-- }
+-- require("material-landscape").setup()
+  xplr.config.general.default_ui.prefix = " "
+  xplr.config.general.default_ui.suffix = ""
+
+  xplr.config.general.focus_ui.prefix = "▸"
+  xplr.config.general.focus_ui.suffix = ""
+  xplr.config.general.focus_ui.style.fg = { Rgb = { 170, 150, 130} }
+  xplr.config.general.focus_ui.style.bg = { Rgb = { 50, 50, 50} }
+  xplr.config.general.focus_ui.style.add_modifiers = { "Bold" }
+
+  xplr.config.general.selection_ui.prefix = " "
+  xplr.config.general.selection_ui.suffix = ""
+  xplr.config.general.selection_ui.style.fg = { Rgb = { 70, 70, 70} }
+  xplr.config.general.selection_ui.style.add_modifiers = { "Bold", "CrossedOut" }
+
+  xplr.config.general.focus_selection_ui.prefix = "▸"
+  xplr.config.general.focus_selection_ui.suffix = ""
+  xplr.config.general.focus_selection_ui.style.fg = { Rgb = { 170, 150, 130 } }
+  xplr.config.general.focus_selection_ui.style.bg = { Rgb = { 50, 50, 50 } }
+  xplr.config.general.focus_selection_ui.style.add_modifiers = { "Bold", "CrossedOut" }
+
+  xplr.config.general.sort_and_filter_ui.separator.format = " » "
+  xplr.config.general.sort_and_filter_ui.separator.style.add_modifiers = { "Bold", "Reversed" }
+
+  xplr.config.general.panel_ui.default.title.style.bg = { Rgb = {170, 150, 130} }
+  xplr.config.general.panel_ui.default.title.style.fg = { Rgb = {40, 40, 40} }
+  xplr.config.general.panel_ui.default.title.style.add_modifiers = { "Bold" }
+  xplr.config.general.panel_ui.default.style.fg = { Rgb = { 170, 150, 130 } }
+  xplr.config.general.panel_ui.default.style.bg = { Rgb = { 33, 33, 33 } }
+  xplr.config.general.panel_ui.default.borders = {}
+  xplr.config.general.panel_ui.table.style.bg = { Rgb = { 26, 26, 26 } }
+
+  xplr.config.general.enable_mouse = true
