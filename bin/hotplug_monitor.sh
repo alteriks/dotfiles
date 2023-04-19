c59e0c23c24f3242af2bb08f2eb81f6e7828bf41
Krzysztof Dajka <alteriks@protonmail.com>
2023-04-19 20:38:36 +0200

    auto-commit moar

--- bin/hotplug_monitor.sh
+++ bin/hotplug_monitor.sh
@@ -5,31 +5,30 @@ export DISPLAY=:0
 export XAUTHORITY=/home/$X_USER/.Xauthority
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u ${X_USER})/bus
 
-function connect()
-{   
-  #/usr/bin/autorandr off
-  sleep 1
-  /usr/bin/autorandr -c 
+function connect() {
+	#/usr/bin/autorandr off
+	sleep 1
+	/usr/bin/autorandr -c
 }
 
-function disconnect(){
-  #/usr/bin/autorandr -l laptop
-  /home/${X_USER}/bin/screenlayout/laptop.sh
+function disconnect() {
+	#/usr/bin/autorandr -l laptop
+	/home/${X_USER}/bin/screenlayout/laptop.sh
 }
 
-if [ $(cat /sys/class/drm/card0-{DP,HDMI,VGA}*/status | grep -c '^connected') -ge 1 ] ; then
-  DISPLAY_SETUP=`autorandr 2>&1 | grep detected | cut -f1 | awk '{print $1}'`
-  if [[ ${DISPLAY_SETUP} =~ 'docked' ]]; then
-    #TODO: qualia ${DISPLAY_SETUP} < ~/.config/i3/config_orig > ~/.config/i3/config
-    sleep 1
-    i3-msg restart
-  fi
-  sleep 1
-  connect
+if [ $(cat /sys/class/drm/card0-{DP,HDMI,VGA}*/status | grep -c '^connected') -ge 1 ]; then
+	DISPLAY_SETUP=$(autorandr 2>&1 | grep detected | cut -f1 | awk '{print $1}')
+	if [[ ${DISPLAY_SETUP} =~ 'docked' ]]; then
+		#TODO: qualia ${DISPLAY_SETUP} < ~/.config/i3/config_orig > ~/.config/i3/config
+		sleep 1
+		i3-msg restart
+	fi
+	sleep 1
+	connect
 
-elif [ $(cat /sys/class/drm/card0-{DP,HDMI,VGA}*/status | grep -c '^connected') -eq 0 ] ; then
-  sleep 1
-  disconnect
-else 
-  exit
+elif [ $(cat /sys/class/drm/card0-{DP,HDMI,VGA}*/status | grep -c '^connected') -eq 0 ]; then
+	sleep 1
+	disconnect
+else
+	exit
 fi
