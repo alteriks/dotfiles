2641b995321102877a78e43d997794b6a656348b
Krzysztof Dajka <alteriks@protonmail.com>
2023-05-26 10:55:46 +0200

    auto-commit moar

--- bin/launch_sway.sh
+++ bin/launch_sway.sh
@@ -5,5 +5,5 @@ export QT_QPA_PLATFORM=wayland
 # https://github.com/flatpak/xdg-desktop-portal-gtk/issues/332#issuecomment-968121867
 export XDG_CURRENT_DESKTOP=sway:GNOME
 export XDG_SESSION_DESKTOP=sway
-exec sway -V
+exec sway -V > /tmp/sway.log 2>&1
 # exec sway -d > /tmp/sway.log 2>&1
