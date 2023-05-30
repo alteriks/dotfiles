7ab92478b55f533d6fe7d2520a08653ed02e6cff
Krzysztof Dajka <alteriks@protonmail.com>
2023-05-30 08:05:59 +0200

    auto-commit moar

--- .config/nvim_lazy/lua/config/options.lua
+++ .config/nvim_lazy/lua/config/options.lua
@@ -6,3 +6,6 @@ local opt = vim.opt
 
 opt.spelllang = { "pl", "en" }
 opt.wrap = true
+
+-- If echasnovski/mini.comment can't use computed LSP ts_context_commentstring then use this:
+opt.commentstring = "# %s"
