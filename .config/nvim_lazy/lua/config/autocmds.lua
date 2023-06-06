fb4ef2fc6f983cb6779f7c680acb11ace0461a72
Krzysztof Dajka <alteriks@protonmail.com>
2023-06-06 21:20:22 +0200

    auto-commit moar

--- .config/nvim_lazy/lua/config/autocmds.lua
+++ .config/nvim_lazy/lua/config/autocmds.lua
@@ -1,3 +1,25 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+if vim.fn.executable("cb") == 0 then
+  print("cb not found, clipboard integration won't work")
+else
+  print("cb found, clipboard integration OK")
+  vim.g.clipboard = {
+    name = "cb (Slackadays/Clipboard)",
+    copy = {
+      ["+"] = "cb copy 0",
+      ["*"] = "cb copy 0",
+    },
+    paste = {
+      ["+"] = function()
+        return vim.fn.systemlist("cb paste 0", { "" }, 1) -- '1' keeps empty lines
+      end,
+      ["*"] = function()
+        return vim.fn.systemlist("cb paste 0", { "" }, 1)
+      end,
+    },
+    cache_enabled = true,
+  }
+end
