e49a8ad81edbc76ca2bd61ef1f8c9e3483b4739c
Krzysztof Dajka <alteriks@protonmail.com>
2023-06-06 21:14:47 +0200

    auto-commit moar

--- .config/nvim_lazy/lua/config/clipboard.lua
+++ .config/nvim_lazy/lua/config/clipboard.lua
@@ -1,18 +1,19 @@
 if vim.fn.executable("cb") == 0 then
-  print("clipboard not found, clipboard integration won't work")
+  print("cb not found, clipboard integration won't work")
 else
+  print("cb found, clipboard integration OK")
   vim.g.clipboard = {
-    name = "clipboard (wsl)",
+    name = "cb (Slackadays/Clipboard)",
     copy = {
-      ["+"] = "cb copy",
-      ["*"] = "cb copy",
+      ["+"] = "cb copy 0",
+      ["*"] = "cb copy 0",
     },
     paste = {
       ["+"] = function()
-        return vim.fn.systemlist("cb paste", { "" }, 1) -- '1' keeps empty lines
+        return vim.fn.systemlist("cb paste 0", { "" }, 1) -- '1' keeps empty lines
       end,
       ["*"] = function()
-        return vim.fn.systemlist("cb paste", { "" }, 1)
+        return vim.fn.systemlist("cb paste 0", { "" }, 1)
       end,
     },
     cache_enabled = true,
