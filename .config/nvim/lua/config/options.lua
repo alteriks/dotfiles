9c7961163018cc1dba917ec7ae87e58fb514fe17
Krzysztof Dajka <alteriks@protonmail.com>
2023-11-13 21:30:56 +0100

    auto-commit moar

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -12,3 +12,15 @@ opt.commentstring = "# %s"
 
 local lazy_core = require("lazy.core.config").options
 lazy_core.lockfile = vim.fn.stdpath("config") .. "/lazy-lock_" .. vim.fn.hostname() .. ".json"
+--
+-- vim.g.clipboard = {
+--   name = "OSC 52",
+--   copy = {
+--     ["+"] = require("vim.clipboard.osc52").copy,
+--     ["*"] = require("vim.clipboard.osc52").copy,
+--   },
+--   paste = {
+--     ["+"] = require("vim.clipboard.osc52").paste,
+--     ["*"] = require("vim.clipboard.osc52").paste,
+--   },
+-- }
