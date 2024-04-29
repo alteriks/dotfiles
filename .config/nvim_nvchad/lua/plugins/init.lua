fc49f88d7c69ca3242cf160140696c911c0f1aaa
Krzysztof Dajka <alteriks@protonmail.com>
2024-04-29 20:50:43 +0200

    auto-commit carbon

---
+++ .config/nvim_nvchad/lua/plugins/init.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "ansible-language-server",
+        "ansiblels",
+        "ansible-lint",
+        "prettier",
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+      },
+    },
+  },
+}
