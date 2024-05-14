258ab0a1e0d1a3e5b950e2a09f5076dcc51a8dee
Krzysztof Dajka <alteriks@protonmail.com>
2024-05-14 16:53:22 +0200

    auto-commit moar

--- .config/nvim/init_bare.lua
+++
@@ -1,5 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
--- require("/home/alteriks/.config/nvim_lazy/bare.lua")
-require("bare.init")
--- .config/nvim/lua/config/mason.lua
+++
@@ -1,8 +0,0 @@
-require("mason").setup({
-  ui = {
-    border = "rounded",
-  },
-})
-require('lint').ansible-lint = {
-  args = {'-c', '~/.config/ansible-lint/config'},
-}
