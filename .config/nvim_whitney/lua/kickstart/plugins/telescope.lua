e017d403486ee8d4c4c577d95fc15ce5581d2af5
Krzysztof Dajka <alteriks@protonmail.com>
2024-08-27 17:44:14 +0200

    auto-commit moar

--- .config/nvim_whitney/lua/kickstart/plugins/telescope.lua
+++ .config/nvim_whitney/lua/kickstart/plugins/telescope.lua
@@ -1,9 +1,19 @@
--- NOTE: Plugins can specify dependencies.
---
--- The dependencies are proper plugin specifications as well - anything
--- you do for a plugin at the top level, you can do for a dependency.
---
--- Use the `dependencies` key to specify the dependencies of a particular plugin
+-- INFO: Open multiple files at once #1048
+-- https://github.com/nvim-telescope/telescope.nvim/issues/1048#issuecomment-1679797700
+local select_one_or_multi = function(prompt_bufnr)
+  local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+  if not vim.tbl_isempty(multi) then
+    require('telescope.actions').close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then
+        vim.cmd(string.format('%s %s', 'edit', j.path))
+      end
+    end
+  else
+    require('telescope.actions').select_default(prompt_bufnr)
+  end
+end
 
 return {
   { -- Fuzzy Finder (files, lsp, etc)
@@ -63,6 +73,7 @@ return {
           mappings = {
             --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
             i = {
+              ['<CR>'] = select_one_or_multi,
               -- ['<ESC>'] = require('telescope.actions').close, -- Exit telescope with single ESC
               ['<C-j>'] = require('telescope.actions').move_selection_next,
               ['<C-k>'] = require('telescope.actions').move_selection_previous,
